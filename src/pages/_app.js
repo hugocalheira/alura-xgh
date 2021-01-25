0b6e1cad5bcd3046262cca028d660320ace3b3a6
Hugo C. Durães <hugocalheira@gmail.com>
2021-01-25 18:48:30 -0300

    Open Graph tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,9 +34,24 @@ export default function App({ Component, pageProps }) {
     <>
       <ThemeProvider theme={theme}>
         <Head>
+          {/* Primary Meta Tags */}
+          <title>{db.title}</title>
+          <meta name="title" content={db.title} />
+          <meta name="description" content={db.description} />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={db.url} />
           <meta property="og:title" content={db.title} />
-          <meta property="og:site_name" content={db.description} />
-          <meta property="og:image" content={db.bg} />
+          <meta property="og:description" content={db.description} />
+          <meta property="og:image" content={db.ogImage} />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content={db.url} />
+          <meta property="twitter:title" content={db.title} />
+          <meta property="twitter:description" content={db.description} />
+          <meta property="twitter:image" content={db.ogImage} />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet" />
